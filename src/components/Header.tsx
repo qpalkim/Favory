f0b92e3f164347dafed4b984e411c69256add840
qpalkim <ghs0199@naver.com>
2025-09-09 13:20:07 +0900

    🎨 style : 불필요한 이미지 파일 제거 및 헤더 컴포넌트 코드 간결화

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
-import logoSm from "@/assets/logo/logo_white_sm.svg";
-import logoMd from "@/assets/logo/logo_white_md.svg";
-import logoLg from "@/assets/logo/logo_white_lg.svg";
+import logo from "@/assets/logo/logo_white.svg";
 
 export default function Header() {
   return (
     <header className="w-full bg-green-500 z-50 shadow-xl">
-      <div className="relative max-w-[1400px] mx-auto lg:h-18 md:h-15 h-13 flex items-center justify-between md:px-18 px-6">
+      <div className="relative max-w-[1400px] mx-auto lg:h-14 md:h-12 h-10 flex items-center justify-between md:px-18 px-6">
         <Link href="/">
-          <Image src={logoSm} alt="모바일 헤더 로고" className="md:hidden " />
           <Image
-            src={logoMd}
-            alt="태블릿 헤더 로고"
-            className="hidden md:block lg:hidden"
+            src={logo}
+            alt="헤더 로고"
+            className="w-[73px] md:w-[92px] lg:w-[110px] hover:opacity-80 transition-opacity"
           />
-          <Image src={logoLg} alt="PC 헤더 로고" className="hidden lg:block" />
         </Link>
-        <Link href="/login">
-          <h4 className="font-semibold text-white text-md md:text-2lg">
-            로그인
-          </h4>
+        <Link
+          href="/login"
+          className="font-semibold text-white text-md md:text-lg lg:text-2lg hover:opacity-80 transition-opacity"
+          aria-label="로그인 페이지로 이동"
+        >
+          로그인
         </Link>
       </div>
     </header>
