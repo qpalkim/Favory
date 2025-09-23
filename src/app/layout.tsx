9b3980b06fc63ae7cfdaf5d114c7784d256817fa
qpalkim <qpalkim.dev@gmail.com>
2025-09-23 13:00:46 +0900

    🎨 style : 푸터 컴포넌트 배치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import { Pretendard, LeferiBold } from "@/font";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
