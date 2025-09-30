770f53cc49aa405a5381d95c05f38f8e9ef6ed6e
qpalkim <qpalkim.dev@gmail.com>
2025-09-30 11:50:18 +0900

    ✨ feat : 로그인 폼 UI 제작

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,11 @@
+import LoginForm from "./_components/LoginForm";
+
 export default function Page() {
-  return <div>로그인 페이지</div>;
+  return (
+    <section className="bg-gradient-to-b from-green-600 via-green-500 to-green-100">
+      <div className="px-4 py-28">
+        <LoginForm />
+      </div>
+    </section>
+  );
 }
