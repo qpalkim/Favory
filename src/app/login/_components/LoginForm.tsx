5a12c8833c80852aa50b81cb56964c49c709531e
Hwisong Kim <120151572+qpalkim@users.noreply.github.com>
2025-10-03 10:22:05 +0900

    feat/FAVORY-11-로그인-폼-컴포넌트 (#8)
    * 💄 design : 텍스트 그림자 변수 추가
    
    * ✨ feat : 로그인 폼 UI 제작
    
    * 💄 design : 높이 값 수정
    
    * ♻️ refactor : 코드 리팩토링 및 랜딩 페이지 이동 추가

---
+++ src/app/login/_components/LoginForm.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+import logo from "@/assets/logo/logo.svg";
+
+export default function LoginForm() {
+  return (
+    <main className="mx-auto max-w-[660px] min-w-[344px] rounded-[16px] bg-white shadow-lg">
+      <form className="space-y-12 px-5 py-10 md:px-6 md:py-15 lg:px-10 lg:py-18">
+        <div className="mb-11 flex flex-col items-center">
+          <Link href="/" className="inline-block text-center">
+            <Image
+              src={logo}
+              alt="로고 아이콘"
+              className="w-16 drop-shadow-lg md:w-20 lg:w-26"
+            />
+            <h1 className="font-leferiBold hover-text-shadow mt-3 text-center text-[18px] text-green-600 md:text-xl md:text-[24px] lg:mt-5 lg:text-[28px]">
+              Favory
+            </h1>
+          </Link>
+          <p className="text-md lg:text-2lg mt-3 text-center md:text-lg lg:mt-4">
+            오늘도 만나서 반가워요!
+          </p>
+        </div>
+        <div className="space-y-5">
+          <Input placeholder="이메일을 입력해 주세요" label="이메일" required />
+          <Input
+            placeholder="비밀번호를 입력해 주세요"
+            type="password"
+            label="비밀번호"
+            required
+          />
+        </div>
+        <div>
+          <Button className="mb-2 w-full" type="submit">
+            로그인하기
+          </Button>
+          <Button variant="outline" className="w-full">
+            Google 간편 로그인하기
+          </Button>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="bg-black-100 h-px flex-1"></div>
+          <p className="lg:text-md text-center text-sm">
+            회원이 아니신가요?&nbsp;
+            <Link
+              href="/signup"
+              className="hover-text-shadow font-semibold text-green-600 underline"
+            >
+              회원가입하기
+            </Link>
+          </p>
+          <div className="bg-black-100 h-px flex-1"></div>
+        </div>
+      </form>
+    </main>
+  );
+}
