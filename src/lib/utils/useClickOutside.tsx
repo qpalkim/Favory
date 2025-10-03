0a4f4b3d4fcf3e534a047543e49489a336aa3bca
Hwisong Kim <120151572+qpalkim@users.noreply.github.com>
2025-10-03 11:25:47 +0900

    feat/FAVORY-13-드롭다운-컴포넌트 (#9)
    * ✨ feat : useClickOutside 유틸 함수 생성
    
    * ✨ feat : 공통 컴포넌트 드롭다운 제작
    
    * 💄 design : 호버 스타일 업데이트

---
+++ src/lib/utils/useClickOutside.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+export function useClickOutside(
+  ref: React.RefObject<HTMLElement | null>,
+  handler: () => void,
+) {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        handler();
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, [ref, handler]);
+}
