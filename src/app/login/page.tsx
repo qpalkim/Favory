11648cc4604701d9d68808f3f21b90219823d600
qpalkim <qpalkim.dev@gmail.com>
2025-10-16 14:14:09 +0900

    ♻️ refactor : 마진 업데이트

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "./_components/LoginForm";
 export default function Page() {
   return (
     <section className="bg-gradient-to-b from-green-600 via-green-500 to-green-100">
-      <div className="px-4 py-28">
+      <div className="px-4 py-[52px] md:px-0 md:py-[70px] lg:py-[100px]">
         <LoginForm />
       </div>
     </section>
