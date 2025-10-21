85e8f23d3eaae6fd55229340b627768cb9b990d9
qpalkim <qpalkim.dev@gmail.com>
2025-10-21 11:38:52 +0900

    ♻️ refactor : className prop 제거 및 createPortal 활용

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,15 +1,15 @@
 "use client";
 import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
 import { cn } from "@/lib/cn";
 import { useClickOutside } from "@/lib/utils/useClickOutside";
 
 interface ModalProps {
   children?: React.ReactNode;
   onClose: () => void;
-  className?: string;
 }
 
-export default function Modal({ children, onClose, className }: ModalProps) {
+export default function Modal({ children, onClose }: ModalProps) {
   const ref = useRef<HTMLDivElement>(null);
 
   useClickOutside(ref, onClose);
@@ -21,7 +21,7 @@ export default function Modal({ children, onClose, className }: ModalProps) {
     };
   }, []);
 
-  return (
+  return createPortal(
     <div
       role="dialog"
       aria-modal="true"
@@ -31,11 +31,11 @@ export default function Modal({ children, onClose, className }: ModalProps) {
         ref={ref}
         className={cn(
           "pointer-events-auto w-[320px] rounded-2xl bg-white p-6 md:w-[419px] md:rounded-[20px] md:p-8",
-          className,
         )}
       >
         {children}
       </div>
-    </div>
+    </div>,
+    document.body,
   );
 }
