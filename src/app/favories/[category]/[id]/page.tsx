6cc02d8d35c58919d096a4e561d9056b5595847c
qpalkim <qpalkim.dev@gmail.com>
2025-10-22 14:23:59 +0900

    🐛 build : params 제거

--- src/app/favories/[category]/[id]/page.tsx
+++ src/app/favories/[category]/[id]/page.tsx
@@ -1,13 +1,3 @@
-export default function Page({
-  params,
-}: {
-  params: { category: string; id: string };
-}) {
-  const { category, id } = params;
-
-  return (
-    <div>
-      Favory {category} {id} 상세 페이지
-    </div>
-  );
+export default function Page() {
+  return <div>Favory 상세 페이지</div>;
 }
