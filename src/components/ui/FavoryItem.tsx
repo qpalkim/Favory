afb42bb26368c8d7790c9ddbaba8944a7e1b52ee
qpalkim <qpalkim.dev@gmail.com>
2025-10-24 13:00:03 +0900

    ✨ feat : 페이보리 아이템 컴포넌트 제작

---
+++ src/components/ui/FavoryItem.tsx
@@ -0,0 +1,52 @@
+import { FavoryDetail } from "./FeedCard";
+import Image from "next/image";
+import Link from "next/link";
+import ProfileImg from "./ProfileImg";
+
+interface FavoryItemProps {
+  favoryDetail: FavoryDetail; // 임시 타입 import
+  search?: boolean;
+}
+
+export default function FavoryItem({
+  favoryDetail,
+  search = false,
+}: FavoryItemProps) {
+  return (
+    <Link
+      href={`/favories/${favoryDetail.category}/${favoryDetail.id}`}
+      className="border-black-100 flex cursor-pointer gap-[10px] overflow-hidden border-b py-4 last:border-b-0 md:gap-4 md:px-4 md:py-6 lg:max-w-[660px] lg:p-6"
+    >
+      {search && (
+        <ProfileImg
+          src={favoryDetail.profileImg}
+          className="pointer-events-none flex-shrink-0"
+        />
+      )}
+      <div className="min-w-0 flex-1">
+        <div className="flex items-baseline justify-between">
+          <h2 className="text-black-500 lg:text-2lg text-sm leading-tight font-semibold md:text-lg">
+            {favoryDetail.title}
+          </h2>
+          <p className="text-black-200 lg:text-md truncate text-[10px] leading-tight md:text-xs">
+            {favoryDetail.createdAt}
+          </p>
+        </div>
+        <p className="text-black-200 md:text-md mt-1 truncate text-xs leading-tight lg:text-lg">
+          {favoryDetail.creator} • {favoryDetail.year}
+        </p>
+        <h3 className="text-black-500 lg:text-2lg mt-2 truncate text-sm leading-tight font-medium md:text-lg">
+          {favoryDetail.favoryTitle}
+        </h3>
+        <p className="text-black-500 md:text-md mt-1 truncate text-xs leading-tight lg:text-lg">
+          {favoryDetail.content}
+        </p>
+      </div>
+      <Image
+        src={favoryDetail.coverImg}
+        alt={favoryDetail.title}
+        className="h-[72px] w-auto flex-shrink-0 rounded-sm object-cover md:h-[92px] md:rounded-md lg:h-[100px]"
+      />
+    </Link>
+  );
+}
