e85e1643203688cb64784dc17797f68cd4929831
qpalkim <qpalkim.dev@gmail.com>
2025-10-24 13:03:10 +0900

    ♻️ refactor : interface export 추가 및 Link 태그에 스타일 부여

--- src/components/ui/FeedCard.tsx
+++ src/components/ui/FeedCard.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import ProfileImg from "./ProfileImg";
 
 // 추후 타입 정의 필요
-interface FavoryDetail {
+export interface FavoryDetail {
   id: string;
   category: string;
   title: string; // 작품명
@@ -23,46 +23,47 @@ interface FeedCardProps {
 
 export default function FeedCard({ favoryDetail }: FeedCardProps) {
   return (
-    <Link href={`favories/${favoryDetail.category}/${favoryDetail.id}`}>
-      <div className="relative flex aspect-square h-full min-h-[160px] w-full min-w-[160px] cursor-pointer items-center justify-center overflow-hidden rounded-xl shadow-lg transition-transform duration-200 hover:scale-105">
-        <Image
-          src={favoryDetail.coverImg}
-          alt={favoryDetail.title}
-          className="h-full w-auto object-cover"
-        />
-        <div className="absolute inset-0 flex flex-col justify-end">
-          <div className="relative flex-1">
-            <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent" />
-            <div className="absolute bottom-0 w-full p-3 md:p-4 lg:p-5">
-              <h2 className="lg:text-2lg truncate text-sm leading-tight font-semibold text-white md:text-lg">
-                {favoryDetail.title}
-              </h2>
-              <p className="md:text-md mt-1 truncate text-xs leading-tight text-white lg:text-lg">
-                {favoryDetail.creator} • {favoryDetail.year}
-              </p>
-            </div>
-          </div>
-          <div className="w-full flex-col justify-between bg-white p-3 md:p-4 lg:p-5">
-            <h3 className="lg:text-2lg text-black-500 truncate text-sm leading-tight font-medium md:text-lg">
-              {favoryDetail.favoryTitle}
-            </h3>
-            <p className="text-black-500 text-md mt-1 hidden truncate leading-tight md:block lg:text-lg">
-              {favoryDetail.content}
+    <Link
+      href={`favories/${favoryDetail.category}/${favoryDetail.id}`}
+      className="relative flex aspect-square h-full min-h-[160px] w-full min-w-[160px] cursor-pointer items-center justify-center overflow-hidden rounded-xl shadow-lg transition-transform duration-200 hover:scale-105"
+    >
+      <Image
+        src={favoryDetail.coverImg}
+        alt={favoryDetail.title}
+        className="h-full w-auto object-cover"
+      />
+      <div className="absolute inset-0 flex flex-col justify-end">
+        <div className="relative flex-1">
+          <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent" />
+          <div className="absolute bottom-0 w-full p-3 md:p-4 lg:p-5">
+            <h2 className="lg:text-2lg truncate text-sm leading-tight font-semibold text-white md:text-lg">
+              {favoryDetail.title}
+            </h2>
+            <p className="md:text-md mt-1 truncate text-xs leading-tight text-white lg:text-lg">
+              {favoryDetail.creator} • {favoryDetail.year}
             </p>
-            <div className="mt-1 flex items-center gap-1 md:mt-2 md:gap-2">
-              <ProfileImg
-                src={favoryDetail.profileImg}
-                size="sm"
-                className="pointer-events-none"
-              />
-              <div className="flex min-w-0 flex-col gap-1">
-                <p className="text-black-500 md:text-md truncate text-xs leading-none lg:text-lg">
-                  {favoryDetail.nickname}
-                </p>
-                <p className="text-black-200 lg:text-md truncate text-[9px] leading-none md:text-xs">
-                  {favoryDetail.createdAt}
-                </p>
-              </div>
+          </div>
+        </div>
+        <div className="w-full flex-col justify-between bg-white p-3 md:p-4 lg:p-5">
+          <h3 className="lg:text-2lg text-black-500 truncate text-sm leading-tight font-medium md:text-lg">
+            {favoryDetail.favoryTitle}
+          </h3>
+          <p className="text-black-500 text-md mt-1 hidden truncate leading-tight md:block lg:text-lg">
+            {favoryDetail.content}
+          </p>
+          <div className="mt-1 flex items-center gap-1 md:mt-2 md:gap-2">
+            <ProfileImg
+              src={favoryDetail.profileImg}
+              size="sm"
+              className="pointer-events-none"
+            />
+            <div className="flex min-w-0 flex-col gap-1">
+              <p className="text-black-500 md:text-md truncate text-xs leading-none lg:text-lg">
+                {favoryDetail.nickname}
+              </p>
+              <p className="text-black-200 lg:text-md truncate text-[9px] leading-none md:text-xs">
+                {favoryDetail.createdAt}
+              </p>
             </div>
           </div>
         </div>
