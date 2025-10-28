24add72d5ca46d5beb381abaf4a6d452704b264f
Hwisong Kim <120151572+qpalkim@users.noreply.github.com>
2025-10-28 13:43:45 +0900

    feat/FAVORY-27-프로필-아이템-컴포넌트 (#23)
    * ✨ feat : ProfileItem 컴포넌트 제작
    
    * 🏗️ chore : 폴더 변경
    
    * 💡 docs : 타입 관련 주석 추가
    
    * 🏗️ chore : common 폴더에서 ui 폴더로 재변경

---
+++ src/components/ui/ProfileItem.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import ProfileImg from "./ProfileImg";
+
+// 추후 타입 정의 필요
+interface User {
+  id: number;
+  nickname: string; // 사용자 닉네임
+  bio: string | null; // 사용자 한 줄 소개
+  profileImg: string | null; // 사용자 프로필 이미지
+}
+
+interface ProfileItemProps {
+  user: User;
+}
+
+export default function ProfileItem({ user }: ProfileItemProps) {
+  return (
+    <Link
+      href={`/profile/@${encodeURIComponent(user.nickname)}`}
+      className="border-black-100 block w-full cursor-pointer overflow-hidden border-b py-4 last:border-b-0 md:py-6 lg:max-w-[660px]"
+    >
+      <div
+        className={`flex gap-[10px] md:gap-4 ${user.bio ? "items-start" : "items-center"}`}
+      >
+        <ProfileImg src={user.profileImg} className="pointer-events-none" />
+        <div className="min-w-0 flex-1">
+          <p className="text-black-500 lg:text-2lg truncate text-sm leading-tight font-semibold md:text-lg">
+            {user.nickname}
+          </p>
+          {user.bio && (
+            <p className="text-black-200 md:text-md mt-1 truncate text-xs leading-tight lg:text-lg">
+              {user.bio}
+            </p>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+}
