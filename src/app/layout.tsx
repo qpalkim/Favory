a9c6161f310428b83d5bbcb940ce98ff644abcd2
qpalkim <qpalkim.dev@gmail.com>
2025-11-24 13:41:42 +0900

    📦 chore : 토스트 라이브러리 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import { ToastContainer } from "react-toastify";
 import { Pretendard, LeferiBold } from "@/font";
 import QueryClientProvider from "@/lib/network/QueryClientProvider";
 import Header from "@/components/ui/Header";
@@ -18,6 +19,28 @@ export default function RootLayout({
     <html lang="ko" className={`${Pretendard.variable} ${LeferiBold.variable}`}>
       <body className="flex h-screen flex-col bg-[#fafafa]">
         <QueryClientProvider>
+          <ToastContainer
+            position="bottom-center"
+            toastStyle={{
+              minHeight: "unset",
+              minWidth: "unset",
+              maxWidth: "80vw",
+              marginBottom: "30px",
+              backgroundColor: "#076653",
+              color: "#ffffff",
+              borderRadius: "16px",
+            }}
+            toastClassName="w-fit flex justify-center text-sm md:text-md break-words"
+            autoClose={2000}
+            icon={false}
+            closeButton={false}
+            pauseOnHover={false}
+            limit={3}
+            hideProgressBar
+            draggable
+            closeOnClick
+            newestOnTop
+          />
           <Header />
           <main className="z-0 flex-grow pt-10 md:pt-12">{children}</main>
           <Footer />
