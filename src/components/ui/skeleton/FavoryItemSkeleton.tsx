d060ebe645f18fbd95cedcd98f5d8fd2a26cb60c
qpalkim <qpalkim.dev@gmail.com>
2025-11-26 12:06:11 +0900

    💄 design : 스켈레톤 UI 살짝 수정

--- src/components/ui/skeleton/FavoryItemSkeleton.tsx
+++ src/components/ui/skeleton/FavoryItemSkeleton.tsx
@@ -1,14 +1,22 @@
-export default function FavoryItemSkeleton() {
+interface FavoryItemSkeletonProps {
+  profile?: boolean;
+}
+
+export default function FavoryItemSkeleton({
+  profile = false,
+}: FavoryItemSkeletonProps) {
   return (
     <div className="border-black-100 flex gap-[10px] overflow-hidden border-b py-4 last:border-b-0 md:gap-4 md:py-6 lg:max-w-[660px]">
-      <div className="bg-black-200 h-[24px] w-[24px] animate-pulse rounded-full md:h-[32px] md:w-[32px]" />
+      {!profile && (
+        <div className="bg-black-100 h-[24px] w-[24px] animate-pulse rounded-full md:h-[32px] md:w-[32px]" />
+      )}
       <div className="min-w-0 flex-1">
-        <div className="bg-black-200 h-[15px] w-[92px] animate-pulse rounded-md md:h-[18px] md:w-[148px]" />
-        <div className="bg-black-200 mt-1 h-[14px] w-[52px] animate-pulse rounded-md md:h-[15px] md:w-[112px]" />
-        <div className="bg-black-200 mt-2 h-[15px] w-[124px] animate-pulse rounded-lg md:h-[18px] md:w-[200px]" />
-        <div className="bg-black-200 mt-1 h-[14px] w-[72px] animate-pulse rounded-lg md:h-[15px] md:w-[92px]" />
+        <div className="bg-black-100 h-[15px] w-[92px] animate-pulse rounded-md md:h-[18px] md:w-[148px]" />
+        <div className="bg-black-100 mt-1 h-[14px] w-[52px] animate-pulse rounded-md md:h-[15px] md:w-[112px]" />
+        <div className="bg-black-100 mt-2 h-[15px] w-[124px] animate-pulse rounded-lg md:h-[18px] md:w-[200px]" />
+        <div className="bg-black-100 mt-1 h-[14px] w-[72px] animate-pulse rounded-lg md:h-[15px] md:w-[92px]" />
       </div>
-      <div className="bg-black-200 flex h-[72px] w-[72px] animate-pulse flex-col items-center justify-center rounded-sm object-cover md:h-[92px] md:w-[92px] md:rounded-md" />
+      <div className="bg-black-100 flex h-[72px] w-[72px] animate-pulse flex-col items-center justify-center rounded-sm object-cover md:h-[92px] md:w-[92px] md:rounded-md" />
     </div>
   );
 }
--- src/components/ui/skeleton/FeedCardSkeleton.tsx
+++ src/components/ui/skeleton/FeedCardSkeleton.tsx
@@ -10,9 +10,9 @@ export default function FeedCardSkeleton() {
           </div>
         </div>
         <div className="w-full flex-col justify-between bg-white p-3 md:p-4 lg:p-5">
-          <div className="bg-black-200 h-[15px] w-[100px] animate-pulse rounded-lg md:h-[18px] md:w-[148px]" />
-          <div className="bg-black-200 mt-1 hidden h-[14px] w-[52px] animate-pulse rounded-lg md:block md:h-[15px] md:w-[92px]" />
-          <div className="bg-black-200 mt-1 h-[24px] w-[24px] animate-pulse rounded-full md:mt-2 md:h-[32px] md:w-[32px]" />
+          <div className="bg-black-100 h-[15px] w-[100px] animate-pulse rounded-lg md:h-[18px] md:w-[148px]" />
+          <div className="bg-black-100 mt-1 hidden h-[14px] w-[52px] animate-pulse rounded-lg md:block md:h-[15px] md:w-[92px]" />
+          <div className="bg-black-100 mt-1 h-[24px] w-[24px] animate-pulse rounded-full md:mt-2 md:h-[32px] md:w-[32px]" />
         </div>
       </div>
     </div>
