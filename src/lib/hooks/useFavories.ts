917f277da4540ede18154e0827181e5b206da6d1
qpalkim <qpalkim.dev@gmail.com>
2025-12-01 18:18:49 +0900

    ✨ feat : 감상평 등록 타입 정의/API 호출/커스텀 훅 제작

--- src/lib/hooks/useFavories.ts
+++ src/lib/hooks/useFavories.ts
@@ -1,6 +1,16 @@
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import { FavoryListResponse, GetFavoryListParams } from "../types/favories";
-import { getFavoryList } from "../apis/favories";
+import {
+  keepPreviousData,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import {
+  AddFavoryRequest,
+  AddFavoryResponse,
+  FavoryListResponse,
+  GetFavoryListParams,
+} from "../types/favories";
+import { addFavory, getFavoryList } from "../apis/favories";
 
 // Favory 목록 조회 훅
 export const useFavoryList = (params: GetFavoryListParams) => {
@@ -8,5 +18,16 @@ export const useFavoryList = (params: GetFavoryListParams) => {
     queryKey: ["favories", params],
     queryFn: () => getFavoryList(params),
     placeholderData: keepPreviousData,
+  });
+};
+
+// Favory 등록 요청 훅
+export const useAddFavory = () => {
+  const queryClient = useQueryClient();
+  return useMutation<AddFavoryResponse, unknown, AddFavoryRequest>({
+    mutationFn: addFavory,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["favories"] });
+    },
   });
 };
