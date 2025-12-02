39656ab2fabf12a0139574975a3e72dc4fae4590
qpalkim <qpalkim.dev@gmail.com>
2025-12-02 15:00:05 +0900

    🚚 chore : 이미지 도메인 설정

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.scdn.co",
       },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+      {
+        protocol: "http",
+        hostname: "books.google.com",
+      },
     ],
   },
 };
