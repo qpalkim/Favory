f213be41d28d6f2bd39393ba52a333f93c0812c4
qpalkim <qpalkim.dev@gmail.com>
2025-12-08 12:48:23 +0900

    ✨ feat : 배너 슬라이드&스와이프&페이지네이션 적용

--- src/components/common/BannerWrapper.tsx
+++ src/components/common/BannerWrapper.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { useFavoryList } from "@/lib/hooks/useFavories";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade, Pagination } from "swiper/modules";
 import Banner from "./Banner";
 import BannerSkeleton from "../skeleton/BannerSkeleton";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
 
 export default function BannerWrapper() {
   const { data, isLoading, isError } = useFavoryList({
@@ -9,10 +14,29 @@ export default function BannerWrapper() {
   });
   if (isLoading) return <BannerSkeleton />;
   if (isError) return <div>에러가 발생했습니다</div>;
+  const favories = data?.content?.slice(0, 4) ?? [];
 
   return (
-    <>
-      {data?.content[0] && <Banner type="latest" favory={data?.content[0]} />}
-    </>
+    <Swiper
+      modules={[Autoplay, EffectFade, Pagination]}
+      slidesPerView={1}
+      effect="fade"
+      speed={800}
+      rewind={true}
+      fadeEffect={{ crossFade: true }}
+      pagination={{ clickable: true }}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      }}
+      className="banner-swiper"
+    >
+      {favories.map((favory) => (
+        <SwiperSlide key={favory.id}>
+          <Banner type="latest" favory={favory} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
