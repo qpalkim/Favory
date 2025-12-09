62844fe8207cd6d048f42bfdf463cfa1bdf73bd2
qpalkim <qpalkim.dev@gmail.com>
2025-12-09 13:19:42 +0900

    ✨ feat : 타입 정의/API 호출/커스텀 훅 제작

--- src/lib/types/comments.ts
+++ src/lib/types/comments.ts
@@ -6,11 +6,11 @@ export const commentResponseSchema = z.object({
   favoryId: z.number(),
   userId: z.number(),
   userNickname: z.string(),
-  userImageUrl: z.string(), // 추가
+  userImageUrl: z.string().nullable(),
   content: z.string().min(1),
   createdAt: z.string(),
   updatedAt: z.string(),
-  deletedAt: z.string(),
+  deletedAt: z.string().nullable(),
 });
 
 export type CommentResponse = z.infer<typeof commentResponseSchema>;
@@ -19,3 +19,20 @@ export type CommentResponse = z.infer<typeof commentResponseSchema>;
 export const commentListResponseSchema = z.array(commentResponseSchema);
 
 export type CommentListResponse = z.infer<typeof commentListResponseSchema>;
+
+// 댓글 등록 요청 API 타입
+export const addCommentRequestSchema = z.object({
+  favoryId: z.number(),
+  userId: z.number(),
+  content: z
+    .string()
+    .min(1)
+    .max(100, { message: "100자 이내로 작성해 주세요" }),
+});
+
+export type AddCommentRequest = z.infer<typeof addCommentRequestSchema>;
+
+// 댓글 등록 응답 API 타입
+export const addCommentResponseSchema = commentResponseSchema;
+
+export type AddCommentResponse = z.infer<typeof addCommentResponseSchema>;
