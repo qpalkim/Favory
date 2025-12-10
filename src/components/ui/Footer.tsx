2d4b577e098fe02a0e7184e469275bf7e82e889e
qpalkim <qpalkim.dev@gmail.com>
2025-12-10 13:16:58 +0900

    💄 design : Footer 컴포넌트 높이 값 변경 및 flex 수정

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="z-10 h-[90px] w-full bg-green-600 px-6 py-[30px] md:h-[160px] md:py-[62px]">
-      <div className="mx-auto flex max-w-[1400px] items-center justify-between">
+    <footer className="z-10 flex h-[90px] w-full items-center justify-center bg-green-600 md:h-[120px]">
+      <div className="flex w-full max-w-[1400px] items-center justify-between px-4">
         <Link
           href="/"
           className="font-leferiBold md:text-2lg text-lg text-white"
