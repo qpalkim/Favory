fe0d047315ba2b3c98254a7f708874c119957e2d
qpalkim <qpalkim.dev@gmail.com>
2025-12-13 14:29:37 +0900

    ✨ feat : 내 정보 조회 API 추가

--- src/lib/apis/users.ts
+++ src/lib/apis/users.ts
@@ -1,9 +1,15 @@
 import { axiosClientHelper } from "../network/axiosClientHelper";
 import { safeResponse } from "../network/safeResponse";
-import { UserResponse, userResponseSchema } from "../types/user";
+import { UserResponse, userResponseSchema } from "../types/users";
 
 // 유저 정보 조회 API
 export const getUserData = async (id: number) => {
   const response = await axiosClientHelper.get<UserResponse>(`users/${id}`);
+  return safeResponse(response.data, userResponseSchema);
+};
+
+// 내 정보 조회 API
+export const getMyData = async () => {
+  const response = await axiosClientHelper.get<UserResponse>("/users/me");
   return safeResponse(response.data, userResponseSchema);
 };
