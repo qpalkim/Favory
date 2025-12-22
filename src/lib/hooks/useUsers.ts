9eb34928d5719cb6f7880534520a27aeb96ddacc
qpalkim <qpalkim.dev@gmail.com>
2025-12-22 11:05:53 +0900

    ✨ feat : API 호출/커스텀 훅 제작

--- src/lib/hooks/useUsers.ts
+++ src/lib/hooks/useUsers.ts
@@ -1,6 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
-import { UserResponse } from "../types/users";
-import { getMyData, getUserData } from "../apis/users";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  ProfileImageUrlParams,
+  profileImageUrlRequestSchema,
+  UserResponse,
+} from "../types/users";
+import { getMyData, getUserData, putProfileImageUrl } from "../apis/users";
 
 // 유저 정보 조회 훅
 export const useUserData = (id?: number) => {
@@ -23,5 +27,15 @@ export const useMyData = () => {
     gcTime: 1000 * 60 * 30,
     refetchOnMount: false,
     refetchOnWindowFocus: false,
+  });
+};
+
+// 프로필 이미지 등록/수정 훅
+export const useProfileImageUrl = () => {
+  return useMutation({
+    mutationFn: async (params: ProfileImageUrlParams) => {
+      profileImageUrlRequestSchema.parse(params);
+      return putProfileImageUrl(params);
+    },
   });
 };
