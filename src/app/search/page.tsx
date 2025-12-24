270300de7a50b9ddcf7e499a91fed06a53a4bb37
qpalkim <qpalkim.dev@gmail.com>
2025-12-24 16:19:33 +0900

    🐛 build : Suspense 빌드 오류 수정

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import SearchContainer from "@/components/common/SearchContainer";
 
 export default function Page() {
-  return <SearchContainer />;
+  return (
+    <Suspense fallback={<div>로딩 중입니다</div>}>
+      <SearchContainer />
+    </Suspense>
+  );
 }
