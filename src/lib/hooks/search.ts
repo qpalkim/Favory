34e961b53fd846b524049d269c5a8baac01696d3
qpalkim <qpalkim.dev@gmail.com>
2025-12-24 16:00:26 +0900

    ✨ feat : 검색 조회 API 호출/커스텀 훅 제작

---
+++ src/lib/hooks/search.ts
@@ -0,0 +1,15 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import {
+  GetSearchFavoryListParams,
+  SearchFavoryListResponse,
+} from "../types/search";
+import { getSearchFavoryList } from "../apis/search";
+
+// Favory 검색 조회 훅
+export const useSearchFavoryList = (params: GetSearchFavoryListParams) => {
+  return useQuery<SearchFavoryListResponse>({
+    queryKey: ["favories", "search", params],
+    queryFn: () => getSearchFavoryList(params),
+    placeholderData: keepPreviousData,
+  });
+};
