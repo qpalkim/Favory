5f01a51f0d3708574bf6024506a5a5131420944c
qpalkim <qpalkim.dev@gmail.com>
2025-12-26 14:03:46 +0900

    ♻️ refactor : 태그 뱃지 라우트 및 페이징 관련 로직 리팩토링

--- src/components/common/SearchContainer.tsx
+++ src/components/common/SearchContainer.tsx
@@ -5,7 +5,11 @@ import { ArrowLeft } from "lucide-react";
 import { Category } from "@/lib/types/search";
 import { UserResponse } from "@/lib/types/users";
 import { Favory } from "@/lib/types/favories";
-import { useSearchFavoryList } from "@/lib/hooks/useSearch";
+import {
+  useDeleteRecentSearchList,
+  useRecentSearchList,
+  useSearchFavoryList,
+} from "@/lib/hooks/useSearch";
 import useMediaQuery from "@/lib/utils/useMediaQuery";
 import SearchBar from "../ui/SearchBar";
 import FavoryItem from "../ui/FavoryItem";
@@ -14,6 +18,7 @@ import Button from "../ui/Button";
 import Badge from "../ui/Badge";
 import SelectOption from "../ui/SelectOption";
 import Pagination from "../ui/Pagination";
+import FavoryItemSkeleton from "../skeleton/FavoryItemSkeleton";
 import Empty from "./Empty";
 
 const MEDIA_TYPES: { label: string; value: Category | undefined }[] = [
@@ -23,6 +28,11 @@ const MEDIA_TYPES: { label: string; value: Category | undefined }[] = [
   { label: "드라마", value: "DRAMA" },
   { label: "서적", value: "BOOK" },
   { label: "프로필", value: "PROFILE" },
+];
+
+const SORT_OPTIONS = [
+  { label: "최신순", value: "latest" },
+  { label: "등록순", value: "oldest" },
 ];
 
 export default function SearchContainer() {
@@ -36,25 +46,42 @@ export default function SearchContainer() {
   const isPC = useMediaQuery("(min-width: 1024px)");
   const isTablet = useMediaQuery("(min-width: 768px) and (max-width: 1023px)");
   const itemsPerPage = isPC ? 10 : isTablet ? 8 : 6;
+  const isTagSearch = keyword.startsWith("#");
+
+  const { data, isLoading, isError } = useSearchFavoryList(
+    isTagSearch
+      ? {
+          keyword,
+          sort: sortType,
+        }
+      : {
+          keyword,
+          category,
+          sort: sortType,
+          size: itemsPerPage,
+          page: currentPage - 1,
+        },
+  );
+  const content = data?.content ?? [];
+  const totalPages = data?.totalPages ?? 0;
 
   useEffect(() => {
     setCurrentPage(1);
+
+    if (keyword.startsWith("#")) {
+      setCategory(undefined);
+    }
   }, [keyword]);
 
-  const { data, isLoading, isError } = useSearchFavoryList({
-    keyword: keyword || "",
-    category: category,
-    sort: sortType,
-    size: itemsPerPage,
-    page: currentPage - 1,
-  });
-  const favories = data?.content ?? [];
-  const totalPages = data?.totalPages ?? 0;
+  const isProfileCategory = category === "PROFILE";
+  const favoryList: Favory[] = !isProfileCategory ? (content as Favory[]) : [];
+  const profileList: UserResponse[] = isProfileCategory
+    ? (content as UserResponse[])
+    : [];
 
-  const sortOptions = [
-    { label: "최신순", value: "latest" },
-    { label: "등록순", value: "oldest" },
-  ];
+  const { data: recentSearchList, isLoading: isRecentSearchListLoading } =
+    useRecentSearchList();
+  const { mutate: deleteRecentSearchList } = useDeleteRecentSearchList();
 
   const handleSearch = (term: string) => {
     setCurrentPage(1);
@@ -86,78 +113,111 @@ export default function SearchContainer() {
             <h4 className="text-black-500 md:text-2lg text-[15px] font-medium">
               최근 검색어
             </h4>
-            <button className="text-error-100 text-xs font-medium md:text-sm">
+            <button
+              className="text-error-100 cursor-pointer text-xs font-medium md:text-sm"
+              onClick={() => deleteRecentSearchList()}
+            >
               모두 지우기
             </button>
           </div>
 
-          <div className="mt-3 flex gap-2 md:mt-4">
-            <Badge>검색어</Badge>
+          <div className="mt-3 flex flex-wrap gap-2 md:mt-4">
+            {isRecentSearchListLoading ? (
+              <>
+                {Array.from({ length: 5 }).map((_, idx) => (
+                  <div
+                    key={idx}
+                    className="bg-black-100/40 h-8 w-12 animate-pulse rounded-md"
+                  />
+                ))}
+              </>
+            ) : recentSearchList && recentSearchList.length > 0 ? (
+              recentSearchList.map((recentSearch, idx) => (
+                <Badge
+                  key={`${recentSearch}-${idx}`}
+                  onClick={() => handleSearch(recentSearch)}
+                >
+                  {recentSearch}
+                </Badge>
+              ))
+            ) : (
+              <div className="my-6 flex w-full justify-center">
+                <Empty type="recentSearch" />
+              </div>
+            )}
           </div>
         </div>
 
-        {keyword &&
-          (isLoading ? (
-            <div>로딩 중입니다</div>
-          ) : (
-            <div>
-              <div className="flex items-center justify-between md:hidden">
-                <h4 className="text-black-500 md:text-2lg text-[15px] font-medium">
-                  검색 결과 {data?.totalElements}개
-                </h4>
+        {keyword && (
+          <div>
+            <div className="flex items-center justify-between md:hidden">
+              <h4 className="text-black-500 md:text-2lg text-[15px] font-medium">
+                검색 결과 {data?.totalElements || 0}개
+              </h4>
+              <SelectOption
+                options={SORT_OPTIONS}
+                onSelect={(option) => {
+                  setSortType(option.value as "latest" | "oldest");
+                  setCurrentPage(1);
+                }}
+              />
+            </div>
+
+            <h4 className="text-black-500 md:text-2lg hidden font-medium md:block">
+              검색 결과 {data?.totalElements || 0}개
+            </h4>
+
+            <div className="mt-6 flex items-center justify-between">
+              <div className="flex items-center gap-1">
+                {MEDIA_TYPES.map((item) => (
+                  <Button
+                    key={item.label}
+                    size="sm"
+                    variant={category === item.value ? "primary" : "outline"}
+                    onClick={() => handleCategoryClick(item.value)}
+                  >
+                    {item.label}
+                  </Button>
+                ))}
+              </div>
+              <div className="hidden md:block">
                 <SelectOption
-                  options={sortOptions}
+                  options={SORT_OPTIONS}
                   onSelect={(option) => {
                     setSortType(option.value as "latest" | "oldest");
                     setCurrentPage(1);
                   }}
                 />
               </div>
+            </div>
 
-              <h4 className="text-black-500 md:text-2lg hidden font-medium md:block">
-                검색 결과 {data?.totalElements}개
-              </h4>
-              <div className="mt-6 flex items-center justify-between">
-                <div className="flex items-center gap-1">
-                  {MEDIA_TYPES.map((item) => (
-                    <Button
-                      key={item.label}
-                      size="sm"
-                      variant={category === item.value ? "primary" : "outline"}
-                      onClick={() => handleCategoryClick(item.value)}
-                    >
-                      {item.label}
-                    </Button>
-                  ))}
-                </div>
-                <div className="hidden md:block">
-                  <SelectOption
-                    options={sortOptions}
-                    onSelect={(option) => {
-                      setSortType(option.value as "latest" | "oldest");
-                      setCurrentPage(1);
-                    }}
-                  />
-                </div>
-              </div>
-
-              <div className="mt-6">
-                {favories.length === 0 ? (
-                  <div className="my-12">
+            <div className="mt-6">
+              {isLoading ? (
+                Array.from({ length: 4 }).map((_, idx) => (
+                  <FavoryItemSkeleton key={idx} />
+                ))
+              ) : isProfileCategory ? (
+                profileList.length === 0 ? (
+                  <div className="my-24">
                     <Empty type="search" />
                   </div>
-                ) : category === "PROFILE" ? (
-                  (favories as UserResponse[]).map((profile) => (
+                ) : (
+                  profileList.map((profile) => (
                     <ProfileItem key={profile.id} profile={profile} />
                   ))
-                ) : (
-                  (favories as Favory[]).map((favory) => (
-                    <FavoryItem key={favory.id} favory={favory} />
-                  ))
-                )}
-              </div>
+                )
+              ) : favoryList.length === 0 ? (
+                <div className="my-24">
+                  <Empty type="search" />
+                </div>
+              ) : (
+                favoryList.map((favory) => (
+                  <FavoryItem key={favory.id} favory={favory} />
+                ))
+              )}
             </div>
-          ))}
+          </div>
+        )}
       </div>
 
       {totalPages > 1 && (
