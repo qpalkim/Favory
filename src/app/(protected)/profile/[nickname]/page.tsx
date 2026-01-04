8000739c149c7832546f769e765cc2f852c5c6b1
qpalkim <qpalkim.dev@gmail.com>
2026-01-04 15:12:25 +0900

    ✨ feat : 프로필 페이지 렌더링

--- src/app/(protected)/profile/[nickname]/page.tsx
+++ src/app/(protected)/profile/[nickname]/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { useState } from "react";
+import { ProfileCategory } from "@/lib/types/users";
+import ProfileLayout from "@/components/common/profile/ProfileLayout";
+import ProfileContent from "@/components/common/profile/ProfileContent";
+
 export default function Page() {
-  return <div>프로필 페이지</div>;
+  const [tab, setTab] = useState<ProfileCategory>("MUSIC");
+
+  return (
+    <div className="mx-auto mt-8 min-h-screen max-w-[1200px] p-4 md:mt-16">
+      <ProfileLayout tab={tab} onTabChange={setTab}>
+        <ProfileContent tab={tab} />
+      </ProfileLayout>
+    </div>
+  );
 }
