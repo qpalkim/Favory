adda0419d4eed9e730271c40e4696515dd0ee92a
qpalkim <qpalkim.dev@gmail.com>
2026-01-13 12:15:57 +0900

    🏷️ chore : 응답 타입 수정

--- src/lib/apis/oauth.ts
+++ src/lib/apis/oauth.ts
@@ -1,16 +1,16 @@
 import { axiosClientHelper } from "../network/axiosClientHelper";
 import { safeResponse } from "../network/safeResponse";
+import { LoginResponse, loginResponseSchema } from "../types/auth";
 import { AddOauthRequest, OauthProvider } from "../types/oauth";
-import { UserResponse, userResponseSchema } from "../types/users";
 
 // 구글 간편 로그인 등록 API
 export const addOauthApps = async (
   provider: OauthProvider,
   data: AddOauthRequest,
 ) => {
-  const response = await axiosClientHelper.post<UserResponse>(
+  const response = await axiosClientHelper.post<LoginResponse>(
     `/login/${provider}`,
     data,
   );
-  return safeResponse(response.data, userResponseSchema);
+  return safeResponse(response.data, loginResponseSchema);
 };
