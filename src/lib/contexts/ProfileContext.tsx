815fad2623b3a027abe01afbd324ed1ebff672fc
qpalkim <qpalkim.dev@gmail.com>
2026-01-14 14:52:22 +0900

    ✨ feat : 프로필 페이지 전역 ContextProvider 제작

---
+++ src/lib/contexts/ProfileContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useState } from "react";
+import { ProfileCategory, UserResponse } from "../types/users";
+
+type ProfileContextValue = {
+  user: UserResponse;
+  isMyProfile: boolean;
+  tab: ProfileCategory;
+  setTab: (tab: ProfileCategory) => void;
+};
+
+const ProfileContext = createContext<ProfileContextValue | null>(null);
+
+export const ProfileProvider = ({
+  user,
+  isMyProfile,
+  children,
+}: {
+  user: UserResponse;
+  isMyProfile: boolean;
+  children: React.ReactNode;
+}) => {
+  const [tab, setTab] = useState<ProfileCategory>("MUSIC");
+
+  return (
+    <ProfileContext.Provider
+      value={{
+        user,
+        isMyProfile,
+        tab,
+        setTab,
+      }}
+    >
+      {children}
+    </ProfileContext.Provider>
+  );
+};
+
+export const useProfile = () => {
+  const context = useContext(ProfileContext);
+  if (!context)
+    throw new Error("useProfile must be used within ProfileProvider");
+  return context;
+};
