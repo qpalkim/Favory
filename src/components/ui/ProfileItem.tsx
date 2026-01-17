92195ee5ed3eb1eaf1393e3df451fb398d80063c
qpalkim <qpalkim.dev@gmail.com>
2026-01-17 15:25:26 +0900

    🚸 pref : group-hover 설정

--- src/components/ui/ProfileItem.tsx
+++ src/components/ui/ProfileItem.tsx
@@ -2,15 +2,11 @@ import { UserResponse } from "@/lib/types/users";
 import Link from "next/link";
 import ProfileImage from "./ProfileImage";
 
-interface ProfileItemProps {
-  profile: UserResponse;
-}
-
-export default function ProfileItem({ profile }: ProfileItemProps) {
+export default function ProfileItem({ profile }: { profile: UserResponse }) {
   return (
     <Link
       href={`/profile/@${encodeURIComponent(profile.nickname)}`}
-      className="border-black-100 block w-full cursor-pointer overflow-hidden border-b py-4 last:border-b-0 md:py-6 lg:max-w-[660px]"
+      className="border-black-100 block w-full overflow-hidden border-b py-4 last:border-b-0 md:py-6 lg:max-w-[660px] group"
     >
       <div
         className={`flex gap-[10px] md:gap-4 ${profile.profileMessage ? "items-start" : "items-center"}`}
@@ -20,7 +16,7 @@ export default function ProfileItem({ profile }: ProfileItemProps) {
           className="pointer-events-none"
         />
         <div className="min-w-0 flex-1">
-          <p className="text-black-500 truncate text-sm leading-tight font-semibold md:text-lg hover:underline">
+          <p className="text-black-500 truncate text-sm leading-tight font-semibold md:text-lg group-hover:underline">
             {profile.nickname}
           </p>
           {profile.profileMessage && (
