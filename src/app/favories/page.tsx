4ab716eca2f23296f95963030084d0fd06a45fc6
qpalkim <qpalkim.dev@gmail.com>
2026-02-15 14:47:12 +0900

    🔧 build : 빌드 에러 수정

--- src/app/favories/page.tsx
+++ src/app/favories/page.tsx
@@ -1,11 +1,21 @@
+import { Suspense } from "react";
 import BannerWrapper from "@/components/common/BannerWrapper";
 import FavoryListContainer from "@/components/common/FavoryListContainer";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
 
 export default function Page() {
   return (
     <>
       <BannerWrapper />
-      <FavoryListContainer />
+      <Suspense
+        fallback={
+          <div className="flex min-h-screen items-center justify-center">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <FavoryListContainer />
+      </Suspense >
     </>
   );
 }
